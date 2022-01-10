9f48ca007605e73e70bdc56adbc54ffdf9a4c1a9
ShuozhenZhao <shuozhenzhao2026@u.northwestern.edu>
2022-01-10 14:52:14 -0600

    try

--- extended_numbers.py
+++ extended_numbers.py
@@ -6,27 +6,27 @@
 
 import numpy as np
 import numbers
-
+# testcommit
 class C:
-    
+
     def __init__(self, data):
         self.data = np.array(data)
-    
+
     def __repr__(self):
         return "%s + %s i" %(self.data[0], self.data[1])
-    
+
     def __eq__(self, other):
         return np.allclose(self.data, other.data)
-    
+
     def __add__(self, other):
         return C( self.data + other.data )
-    
+
     def __neg__(self):
         return C( -self.data )
-    
+
     def __sub__(self, other):
         return self + (-other)
-    
+
     def __mul__(self, other):
         if isinstance(other, C):
             return C( (self.data[0]*other.data[0] - self.data[1]*other.data[1],
@@ -35,10 +35,9 @@ class C:
             return C( other*self.data )
         else:
             raise ValueError("Can only multiply complex numbers by other complex numbers or by scalars")
-            
+
     def __rmul__(self, other):
         if isinstance(other, numbers.Number):
             return C( other*self.data )
         else:
-            raise ValueError("Can only multiply complex numbers by other complex numbers or by scalars")    
-
+            raise ValueError("Can only multiply complex numbers by other complex numbers or by scalars")
