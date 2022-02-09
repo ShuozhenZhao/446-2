4abb40cd7fae56b4b5baff8fdb024e6b039d8053
ShuozhenZhao <shuozhenzhao2026@u.northwestern.edu>
2022-02-09 16:11:53 -0600

    setup

---
+++ equations.py
@@ -0,0 +1,22 @@
+
+import numpy as np
+import spectral
+from scipy import sparse
+
+class KdVEquation:
+
+    def __init__(self, domain, u):
+        pass
+
+    def evolve(self, timestepper, dt, num_steps):
+        pass
+
+class SHEquation:
+
+    def __init__(self, domain, u):
+        pass
+
+    def evolve(self, timestepper, dt, num_steps):
+        pass
+
+
